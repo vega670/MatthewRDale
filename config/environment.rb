1bd5d0276b9de9683dda77cab76805764a11df8d
Matthew Dale <vega.net@gmail.com>
2011-07-10 21:07:17 -0400

    Returned project to development state.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MatthewRDale::Application.initialize!
 
-IMAGE_MAGICK_PATH = "C:\Program Files\ImageMagick"
+IMAGE_MAGICK_PATH = "/usr/bin"
