9b8cbdfa17b6ff101a7e96fb4bf0fcd336dd4fda
unknown <Matthew@.(none)>
2011-07-12 01:01:03 -0400

    Changed Pages controller to work with permalink instead of id.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -44,8 +44,8 @@ class PagesController < ApplicationController
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to(@page, :notice => 'Page was successfully created.') }
-        format.xml  { render :xml => @page, :status => :created, :location => @page }
+        format.html { redirect_to(:controller => :pages, :id => @page.permalink, :notice => 'Page was successfully created.') }
+        format.xml  { render :xml => {:controller => :pages, :id => @page.permalink}, :status => :created, :location => @page }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @page.errors, :status => :unprocessable_entity }
@@ -60,7 +60,7 @@ class PagesController < ApplicationController
 
     respond_to do |format|
       if @page.update_attributes(params[:page])
-        format.html { redirect_to(@page, :notice => 'Page was successfully updated.') }
+        format.html { redirect_to(:controller => :pages, :id => @page.permalink, :notice => 'Page was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -72,7 +72,7 @@ class PagesController < ApplicationController
   # DELETE /pages/1
   # DELETE /pages/1.xml
   def destroy
-    @page = Page.find(params[:id])
+    @page = Page.find_by_permalink(params[:id])
     @page.destroy
 
     respond_to do |format|
